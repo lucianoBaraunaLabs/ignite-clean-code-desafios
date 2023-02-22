9d241b77c7723fe7e9807de7510edd4aa9a28429
Luciano Baraúna <baraunaluciano@gmail.com>
2023-02-22 01:02:01 -0300

    feat: completed challenge 02

---
+++ desafios/02-booleanos-refactor.ts
@@ -0,0 +1,31 @@
+// Boleanos
+
+const user = {
+  name: 'Diego Fernandes',
+  height: 190,
+  hasTicket: true,
+}
+
+const necessaryHeight = 130
+
+const currentHour = new Date().getHours()
+
+const isParkOpen = currentHour > 9 && currentHour < 18
+
+if (!isParkOpen) {
+  throw new Error('O parque está fechado!')
+}
+
+const hasUserTicket = user.hasTicket
+
+if (!hasUserTicket) {
+  throw new Error('O Diego não possui um bilhete para entrar no parque!')
+}
+
+const hasEnterToy = user.height > necessaryHeight
+
+if (!hasEnterToy) {
+  throw new Error('O Diego não pode entrar no brinquedo!')
+}
+
+console.log('O Diego se divertiu muito! :)')
